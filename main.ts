fbceaf7776efe2b5c001310a43bd2f5cfdf1dba6
Alban Rochel <albanrochel@hotmail.com>
2021-01-30 22:52:17 +0100

    First test, for use as a Makecode extension

---
+++ main.ts
@@ -0,0 +1,9 @@
+namespace Alban {
+        //% block="This is a test:|x: $x0 y: $y0"
+    //% x0.defl=0
+    //% y0.defl=0
+    //% weight=1
+    export function test(x0: number, y0: number) {
+
+    }
+}
